ae8fd869016617c1a41351c78338161b192692c6
teddybear0430 <teddybear0430@hotmail.com>
2019-11-07 22:34:02 +0900

    404ページを追加。

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/Layout.js';
+
+const NotFoundPage = () => {
+    return (
+        <Layout>
+        <div id="content">
+            <h1>NOT FOUND</h1>
+            <p>お探しのページは見つかりません</p>
+        </div>
+        </Layout>
+    )
+}
+
+export default NotFoundPage;
